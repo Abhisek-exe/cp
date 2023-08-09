637328a07338edf7917ddd4814709d2835274814
Abhisek-exe <121687794+Abhisek-exe@users.noreply.github.com>
2023-08-09 22:51:00 +0530

    first commit

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Animal{
+    public:
+    void speak(){
+        cout<<"speaking"<<endl;
+    }
+
+};
+class dog:public Animal{
+    public:
+    void speak(){
+        cout<<"bartking"<< endl;
+    }
+};
+int main(){
+    dog obj1;
+    obj1.speak();
+}
